4803171c0befb75580fed8a4cd9d7becd9550d33
vladimirSv <59789656+vladovip@users.noreply.github.com>
2022-04-07 14:21:16 +0300

    Delete FinalExamTasks_03APR22 directory
    Need to be deleted

--- FinalExamTasks_03APR22/demo.js
+++
@@ -1,7 +0,0 @@
-
-let pattern2 = /[A-Z]+/g
-      let isValid = pattern2.test(password)
-    //   console.log(match);
-      if (isValid !== true) {
-        console.log("Password must consist at least one uppercase letter!");
-      }
